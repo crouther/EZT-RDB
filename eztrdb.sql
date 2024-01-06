4b881e6df0e24dc0739f4e510794767fde3a0dd4
crouther <33792608+crouther@users.noreply.github.com>
2024-01-06 12:36:54 -0500

    Create eztrdb.sql
    high fidelity pseudo code for sql tables required for our relational database.

---
+++ eztrdb.sql
@@ -0,0 +1,20 @@
+// Myles Crouther, David Willoughby, Jonathan Stuppin
+// University of Arizona Global Campus
+// INT 499 - Capstone for Information Technology (INP2350A)
+// Professor: Joseph Issa
+
+
+Table users {
+  id integer [primary key]
+  username varchar
+  role varchar
+  created_at timestamp
+}
+
+Table content {
+  id integer [primary key]
+  title varchar
+  body text [note: 'Footage Description']
+  runtime integer
+  created_at timestamp
+}
